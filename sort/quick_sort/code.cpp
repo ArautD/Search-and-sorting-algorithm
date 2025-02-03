e74da114a682979346668fddb3c7bc596525a7be
ARAUT <sergiosg6@yahoo.com>
2025-02-03 17:13:07 -0300

    add counting sort

--- sort/quick_sort/code.cpp
+++ sort/quick_sort/code.cpp
@@ -5,9 +5,10 @@ using namespace std;
 /**
  * Método para fazer a troca de dados entre duas posições no vetor
  */
-void troca(int i, int j, int vet[]){
+void troca(int i, int j, int vet[])
+{
     int k = vet[i];
-    vet[i]=vet[j];
+    vet[i] = vet[j];
     vet[j] = k;
 }
 
@@ -15,14 +16,17 @@ void troca(int i, int j, int vet[]){
  * Método de particionamento que é o núcleo do algoritmo Quicksort.
  * É a implementação utilizando o paradigma dividir para conquistar
  */
-int particionamento(int vet[], int p, int q){
+int particionamento(int vet[], int p, int q)
+{
     int i = p;
-    for(int j = p; j<q; j++){
-        if(vet[j]<=vet[q]){
-            troca(i++,j, vet);
+    for (int j = p; j < q; j++)
+    {
+        if (vet[j] <= vet[q])
+        {
+            troca(i++, j, vet);
         }
     }
-    troca(i,q, vet);
+    troca(i, q, vet);
 
     return i;
 }
@@ -30,31 +34,32 @@ int particionamento(int vet[], int p, int q){
 /**
  * Método que implementa o Quicksort recursivamente
  */
-void quicksort(int vet[], int p, int r){
-    if(p >= r){
+void quicksort(int vet[], int p, int r)
+{
+    if (p >= r)
+    {
         return;
     }
     int pivo = particionamento(vet, p, r);
 
-    quicksort(vet, p, pivo-1);
-    quicksort(vet, pivo+1, r);
-
+    quicksort(vet, p, pivo - 1);
+    quicksort(vet, pivo + 1, r);
 }
 
 int main()
 {
     /**
      * Testando o quicksort com vetor de inteiros
-     * 
+     *
      */
     int vetor[] = {50, 42, 8, 23, 74, 91, 13, 26, 66, 3};
-    int tamanho = sizeof vetor/sizeof vetor[0];
+    int tamanho = sizeof vetor / sizeof vetor[0];
 
     quicksort(vetor, 0, tamanho - 1);
     for (int i = 0; i < tamanho; i++)
     {
-            cout<<vetor[i]<<",";
+        cout << vetor[i] << ",";
     }
-    
+
     return 0;
 }
